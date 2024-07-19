01b4b328c662db2d15cc85350818db3576bf2bd7
kishan95guleria <91966583+kishan95guleria@users.noreply.github.com>
2024-07-19 16:41:57 +0530

    Update settings.py
    change email password

--- codigo_mantra_test/settings.py
+++ codigo_mantra_test/settings.py
@@ -141,4 +141,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "demowisdom694@gmail.com"
-EMAIL_HOST_PASSWORD = "stpiavxmhxxdcpsr" 
+EMAIL_HOST_PASSWORD = "your_password" 
